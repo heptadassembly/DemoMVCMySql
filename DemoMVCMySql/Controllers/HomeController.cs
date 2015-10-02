92ffa016ee65db55a8e797cb2c48623075098c47
harrisontim7104 <harrisontim7104@gmail.com>
2015-10-02 15:02:49 -0400

    initial
    Basic 101 MVC and MySql

---
+++ DemoMVCMySql/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMVCMySql.Models;
+using System.Data.Objects;
+using System.Web.Helpers;
+
+namespace DemoMVCMySql.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(ProfileModel model, string actionRequest)
+        {
+
+            if (model.UserID != null && model.UserID.Length > 0  && model.Password.Length > 0)
+            {
+                List<profile> profiles = new List<profile>();
+                //Validate login
+
+                using (var queryDB = new samsjacksonvilleEntities())
+                {
+                    profiles = queryDB.profiles.SqlQuery("select * from profile where UserID = @p0 and Password = @p1", model.UserID, model.Password).ToList();
+                }
+
+                
+            }
+            return View();
+
+        }
+    }
+}
---
+++ DemoMVCMySql/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DemoMVCMySql")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("DemoMVCMySql")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("19ef4324-07f5-4617-8c28-df7ceae5494e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
